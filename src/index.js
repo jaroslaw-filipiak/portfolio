3d1cfa530fe7dafd851f90272b4f13a2e584c775
Jaroslaw Filipiak <info@j-filipiak.pl>
2020-01-06 12:11:32 +0100

    test netlify continuous

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/sass/style.scss';
-import Root from './views/Root/Root'
+import Root from './views/Root/Root';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( < Root / > , document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 
-// If  syou want your app to work offline and load faster, you can change
+// If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
