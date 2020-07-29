2b55c73aab4ff0d0fe844b4ada3a162bd989511d
Jarek <info@j-filipiak.pl>
2020-07-29 20:39:28 +0200

    loading indicator

--- src/components/Portfolio/PortfolioList.js
+++ src/components/Portfolio/PortfolioList.js
@@ -3,24 +3,27 @@ import axios from 'axios';
 import PortfolioItem from './PortfolioItem';
 
 export default class PortfolioList extends Component {
-  state = {
-    items: []
-  };
+	state = {
+		items: [],
+		isLoading: false,
+	};
 
-  componentDidMount() {
-    axios
-      .get('https://www.j-filipiak.pl/api/wp-json/wp/v2/portfolio?embded')
-      .then(response => {
-        this.setState({
-          items: response.data
-        });
-      });
-  }
+	componentDidMount() {
+		axios
+			.get('https://www.j-filipiak.pl/api/wp-json/wp/v2/portfolio?embded')
+			.then((response) => {
+				this.setState({
+					items: response.data,
+					isLoading: true,
+				});
+			});
+	}
 
-  render() {
-    const posts = this.state.items.map(item => {
-      return <PortfolioItem key={item.id} item={item} />;
-    });
-    return <> {posts} </>;
-  }
+	render() {
+		const posts = this.state.items.map((item) => {
+			return <PortfolioItem key={item.id} item={item} />;
+		});
+
+		return this.state.isLoading ? posts : 'czekaj...';
+	}
 }
--- src/components/Portfolio/Single.js
+++ src/components/Portfolio/Single.js
@@ -3,27 +3,29 @@ import axios from 'axios';
 import SingleContent from './SingleContent';
 
 export default class Single extends Component {
-  state = {
-    items: []
-  };
+	state = {
+		items: [],
+		isLoading: false,
+	};
 
-  componentDidMount() {
-    console.log(this.props.slug);
-    axios
-      .get(
-        `https://www.j-filipiak.pl/api/wp-json/wp/v2/portfolio?slug=${this.props.slug}}`
-      )
-      .then(response => {
-        this.setState({
-          items: response.data
-        });
-      });
-  }
+	componentDidMount() {
+		console.log(this.props.slug);
+		axios
+			.get(
+				`https://www.j-filipiak.pl/api/wp-json/wp/v2/portfolio?slug=${this.props.slug}}`
+			)
+			.then((response) => {
+				this.setState({
+					items: response.data,
+					isLoading: true,
+				});
+			});
+	}
 
-  render() {
-    const item = this.state.items.map(item => {
-      return <SingleContent key={item.id} item={item} />;
-    });
-    return <> {item} </>;
-  }
+	render() {
+		const item = this.state.items.map((item) => {
+			return <SingleContent key={item.id} item={item} />;
+		});
+		return this.state.isLoading ? item : 'czekaj...';
+	}
 }
